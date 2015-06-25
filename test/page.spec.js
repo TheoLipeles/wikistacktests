2ab850afdc77de54db160ca0f0fd2ebac5ddc314
celanajaya <petermsteele@gmail.com>
2015-06-25 16:19:45 -0400

    search by tags

--- test/page.spec.js
+++ test/page.spec.js
@@ -5,34 +5,71 @@ var Page = require("../models/index.js").Page;
 
 describe("Wikistack Model", function() {
 	describe("Page Schema", function() {
-		describe("Validation", function() {
-			var page;
-			beforeEach(function () {
-				page = new Page();
-			});
-			it("returns true if valid title otherwise returns error", function() {
-				page.title = "validTitle";
-				page.validate(function(err) {
-					expect(err).to.be.undefined;
-					page.title = "";
-					page.validate(function(err) {
-						expect(err.errors).to.have.property("title");
+		var page;
+		var page2;
+		var page3;
+		beforeEach(function (done) {
+			Page.remove({});
+			page = new Page();
+			page2 = new Page();
+			page3 = new Page();
+			page.body = "Somebody...";
+			page.title = "The title!";
+			page2.body = "Somebody2...";
+			page2.title = "The title2!";
+			page3.body = "Somebody3...";
+			page3.title = "The title3!";
+			// page2.tags = ["text1", "text2", 'text3'];
+			// page3.tags = ["text4", "text5", 'text6'];
+			// page.tags = ["text7", "text1", 'text4'];
+			page.save(function(){
+				page2.save(function(){
+					page3.save(function(){
 						done();
 					});
 				});
-				
+			});
+		});
+		afterEach(function(){
+			Page.remove({});
+		});
+		describe("Validation", function() {
+			it("doesn't return error if title is valid", function(done) {
+				page.title = "validTitle";
+				page.validate(function(err) {
+					expect(err).to.be.null;
+					done();
+				});
+			});
+			it("returns error if no valid title", function(done) {	
+				page.title = "";
+				page.validate(function(err) {
+					expect(err.errors).to.have.property("title");
+					done();
+				});
 			});
 		});
 		describe("Methods", function() {
-			describe("computeUrlName", function() {
-				it("replaces whitespace with _ and removes non-alphanumeric chars");
+			describe("computeUrlName", function(done) {
+				it("replaces whitespace with _ and removes non-alphanumeric chars", function(done){
+					page.title = "The Quick F@x";
+					page.computeUrlName();
+					expect(page.url_name).to.equal("The_Quick_F_x");
+					done();
+				});
 			});
 			describe("getSimilar", function() {
-				it("returns similar pages");
+				it("returns pages with same tag", function(done){
+					page.getSimilar(function(err, pages){
+						console.log(pages);
+						expect(pages).to.equal(pages);
+						done();
+					});
+				});
 			});
 		});
 		describe("Hooks", function() {
-			describe("calls computeUrlName before saving");
+			it("calls computeUrlName before saving");
 		});
 		describe("Virtuals", function() {
 			describe("full_route", function() {
